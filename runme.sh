19925731d2838ccf418107993b3e7c818191d731
Github Drone <noreply@shanelongman.com>
2022-02-05 18:19:07 +0100

    Generalize the runner script

--- runme.sh
+++ runme.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e
 
-MATCH=20
-
-./main -i targets.csv ${MATCH} | xz -9 > log1.txt.xz &
-./main -i targets.csv ${MATCH} | xz -9 > log2.txt.xz &
-./main -i targets.csv ${MATCH} | xz -9 > log3.txt.xz &
-./main -i targets.csv ${MATCH} | xz -9 > log4.txt.xz &
+./main ${@} | xz -9 > log1.txt.xz &
+./main ${@} | xz -9 > log2.txt.xz &
+./main ${@} | xz -9 > log3.txt.xz &
+./main ${@} | xz -9 > log4.txt.xz &
 wait
