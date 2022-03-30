f0b6c6ee1eeed086b0aa010098fc593c25f6d4d7
Github Drone <noreply@shanelongman.com>
2022-03-30 22:51:29 +0200

    Tweak runme.sh to accept any executable

--- runme.sh
+++ runme.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
-./main ${@} | xz -9 > log1.txt.xz &
-./main ${@} | xz -9 > log2.txt.xz &
-./main ${@} | xz -9 > log3.txt.xz &
-./main ${@} | xz -9 > log4.txt.xz &
+# Call executable passed as first argument, with remaining
+# arguments passed to it
+
+./${1} ${@: 2} | xz -9 > log1.txt.xz &
+./${1} ${@: 2} | xz -9 > log2.txt.xz &
+./${1} ${@: 2} | xz -9 > log3.txt.xz &
+./${1} ${@: 2} | xz -9 > log4.txt.xz &
 wait
